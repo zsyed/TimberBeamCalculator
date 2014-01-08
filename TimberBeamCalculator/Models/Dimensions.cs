3a733b0e5f71a5705366be21274c991c3c5414ec
zsyed <zulfiqarsyed@yahoo.com>
2014-01-07 19:00:34 -0800

    add calcuate page and add input text part 3

---
+++ TimberBeamCalculator/Models/Dimensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimberBeamCalculator.Models
+{
+    public class Dimensions
+    {
+        public double SpanLength { get; set; }
+        public double VariableLoadSafetyFactor { get; set; }
+        public double PermanentLoadSafetyFactor { get; set; }
+    }
+}
