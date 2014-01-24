e9695824603c9ea7d9d7f2e6d46761cfa5124539
zsyed <zulfiqarsyed@yahoo.com>
2014-01-24 07:01:55 -0800

    recalculating excel formula cell after values are entered.

--- TimberBeamCalculator/Controllers/CalculatorController.cs
+++ TimberBeamCalculator/Controllers/CalculatorController.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.ExtendedProperties;
 
 namespace TimberBeamCalculator.Controllers
 {
@@ -28,16 +29,61 @@ namespace TimberBeamCalculator.Controllers
             return View(dim);
         }
 
+        private void SaveNumberToCell(string fileName, string sheetName, string cellCoordinates, string value)
+        {
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, true))
+            {
+
+
+                Sheet sheet = document.WorkbookPart.Workbook.Descendants<Sheet>().SingleOrDefault(s => s.Name == sheetName);
+                if (sheet == null)
+                {
+                    throw new ArgumentException(
+                        String.Format("No sheet named {0} found in spreadsheet {1}", "myRange1", "myfile"), "sheetName");
+                }
+                WorksheetPart worksheetPart = (WorksheetPart)document.WorkbookPart.GetPartById(sheet.Id);
+
+
+                int rowIndex = int.Parse(cellCoordinates.Substring(1));
+
+                Row row = worksheetPart.Worksheet.GetFirstChild<SheetData>().
+                        Elements<Row>().FirstOrDefault(r => r.RowIndex == rowIndex);
+
+                Cell cell3 = row.Elements<Cell>().FirstOrDefault(c => cellCoordinates.Equals(c.CellReference.Value));
+                if (cell3 != null)
+                {
+                    cell3.CellValue = new CellValue(value);
+                    cell3.DataType = new DocumentFormat.OpenXml.EnumValue<CellValues>(CellValues.Number);
+                }
+
+                worksheetPart.Worksheet.Save();
+                document.Close();
+
+            }
+        }
+
         [HttpPost]
         public ActionResult Index(TimberBeamCalculator.Models.Dimensions d)
         {
-            string filename = Server.MapPath("/") + "TimberBeamData.xlsx";
+            string filename;
+            string path = Server.MapPath("/"); 
+            
+            filename = path + "TimberBeamData.xlsx";
+
+            SaveNumberToCell(filename, "myRange1", "C2", d.PermanentLoadSafetyFactor.ToString());
+            SaveNumberToCell(filename, "myRange1", "C3", d.SpanLength.ToString());
+            SaveNumberToCell(filename, "myRange1", "C4", d.VariableLoadSafetyFactor.ToString());
+
+  
+
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook workbook = excelApp.Workbooks.Open(Path.GetFullPath(filename));
+            workbook.Close(true);
+            excelApp.Quit();
 
             // getting the result out of excel.
-            using (SpreadsheetDocument document = SpreadsheetDocument.Open(filename, true))
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(filename, false))
             {
-                document.WorkbookPart.Workbook.CalculationProperties.ForceFullCalculation = true;
-                document.WorkbookPart.Workbook.CalculationProperties.FullCalculationOnLoad = true;
 
                 Sheet sheet = document.WorkbookPart.Workbook.Descendants<Sheet>().SingleOrDefault(s => s.Name == "myRange1");
                 if (sheet == null)
@@ -45,17 +91,21 @@ namespace TimberBeamCalculator.Controllers
                     throw new ArgumentException(
                         String.Format("No sheet named {0} found in spreadsheet {1}", "myRange1", filename), "sheetName");
                 }
+
+
+
                 WorksheetPart worksheetPart = (WorksheetPart)document.WorkbookPart.GetPartById(sheet.Id);
 
-                int rowIndex = int.Parse("C4".Substring(1));
+                int rowIndex = int.Parse("C5".Substring(1));
 
                 Row row = worksheetPart.Worksheet.GetFirstChild<SheetData>().
                         Elements<Row>().FirstOrDefault(r => r.RowIndex == rowIndex);
- 
-                Cell cell = row.Elements<Cell>().FirstOrDefault(c => "C4".Equals(c.CellReference.Value));
-                d.Average = Convert.ToDouble(cell.CellValue.InnerText);
-            }
 
+                Cell cell = row.Elements<Cell>().FirstOrDefault(c => "C5".Equals(c.CellReference.Value));
+
+                d.FinalResult = Convert.ToDouble(cell.CellValue.InnerText);
+
+            }
 
             return RedirectToAction("Pdf", d);
         }
@@ -68,7 +118,9 @@ namespace TimberBeamCalculator.Controllers
             // With no Model and default view name.  Pdf is always the default view name
             return new PdfResult(d);
         }
+ 
 
     }
 }
+
 
