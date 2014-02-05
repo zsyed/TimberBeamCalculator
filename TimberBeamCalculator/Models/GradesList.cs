816f151a98ec091e5c62290a9eaba22f5b4eb5a7
zsyed <zulfiqarsyed@yahoo.com>
2014-02-04 19:50:24 -0800

    Set Timber Grade text box based on grid selection.

---
+++ TimberBeamCalculator/Models/GradesList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimberBeamCalculator.Models
+{
+    public static class Grades
+    {
+        public static List<TimberGrades> GetList()
+        {
+            var grades = new List<TimberGrades>();
+            grades.Add(new TimberGrades() { TimberGrade = "C14", BendingParallelToGrain = 4.1 });
+            grades.Add(new TimberGrades() { TimberGrade = "C16", BendingParallelToGrain = 5.3 });
+            return grades;
+        }
+    }
+}
